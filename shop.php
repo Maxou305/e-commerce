a472634c1a7858d7fe52de2b8d03bc5cc1a0f81c
lpzkjdoi <maxime.chazard@le-campus-numerique.fr>
2023-09-06 17:00:51 +0200

    étape 7.1 + grid

--- shop.php
+++ shop.php
@@ -38,29 +38,30 @@
         ],
     ];
 ?>
-<?php foreach ($products as $produit) : ?>
-    <div class="produit">
-        <img src="<?php echo $produit['photo']?>" alt="">
-        <div>
-            <h2><?php echo $produit['nom']?></h2>
-            <p>description produit</p>
-            <p class="poids">Poids : <?php echo $produit['poids'] ?> kg</p>
-            <p class="prix-ttc">Prix TTC : <?php formatPrice($produit['prix']) ?></p>
-            <p class="ristourne">RISTOURNE : <?php echo $produit['ristourne'] ?><?php if ($produit['ristourne'] == null) echo "0" ?> %</p>
-            <p class="prix-ristourne-ttc">Prix TTC après ristourne : <?php formatPrice(discountedPrice($produit['prix'], $produit['ristourne'])) ?></p>
-            <label for="quantite">Quantité :</label>
-            <select name="nombre" id="quantite">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-        <a class="cta" href="">Commander</a>
+<div class="shop">
+    <?php foreach ($products as $produit) : ?>
+        <div class="produit">
+            <img src="<?php echo $produit['photo']?>" alt="">
+            <div>
+                <h2><?php echo $produit['nom']?></h2>
+                <p>description produit</p>
+                <p class="poids">Poids : <?php echo $produit['poids'] ?> kg</p>
+                <p class="prix-ttc">Prix TTC : <?php formatPrice($produit['prix']) ?></p>
+                <p class="ristourne">RISTOURNE : <?php echo $produit['ristourne'] ?><?php if ($produit['ristourne'] == null) echo "0" ?> %</p>
+                <p class="prix-ristourne-ttc">Prix TTC après ristourne : <?php formatPrice(discountedPrice($produit['prix'], $produit['ristourne'])) ?></p>
+                <label for="quantite">Quantité :</label>
+                <select name="nombre" id="quantite">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <a class="cta" href="">Commander</a>
+            </div>
         </div>
-    </div>
+    <?php endforeach;?>
+</div>
         
-    </div>
-<?php endforeach;
 
-include('templates/footer.php');
+<?php include('templates/footer.php');?>
